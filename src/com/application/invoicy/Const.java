0c2014ec5aeec161b4933a61f75842f09bc99f75
Enrico Bianchi <enrico.bianchi@gmail.com>
2012-08-09 01:49:54 +0200

    Added comments into const class

--- src/com/application/invoicy/Const.java
+++ src/com/application/invoicy/Const.java
@@ -1,14 +1,27 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2012 Enrico Bianchi <enrico.bianchi@ymail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
  */
+
 package com.application.invoicy;
 
 import java.io.File;
 
 /**
- *
- * @author enrico
+ * Project       Invoicy
+ * Description   An invoice management system
+ * 
+ * @author Enrico Bianchi <enrico.bianchi@ymail.com>
  */
 public class Const {
     
@@ -18,5 +31,5 @@ public class Const {
     
     public final static File CFG_FILE = new File(USER_DIR.getAbsolutePath()
                 + System.getProperty("file.separator")
-                + "settings.cfg");
+                + "settings.cfg");    
 }
